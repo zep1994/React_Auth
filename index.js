19f8054b21cb974cd8564d1dae3bc09db81026e0
zep1994 <snaxbags@protonmail.com>
2019-01-31 17:27:05 -0600

    Init server setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+//App Setup
+
+
+//Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log("server listening on:", port);
